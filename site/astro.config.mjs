c99f7540fd8fecc4ea0d3a00b4d490d021e343d4
Michael Unterkalmsteiner <mun@bth.se>
2026-04-16 23:36:05 +0200

    Config site to use real domain

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   output: 'static',
-  site: 'https://bth-dipt-research.github.io/scipop',
-  base: '/scipop',
+  site: 'https://www.serlsweden.se',
+  base: '/',
 });
